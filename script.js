63dd8d4534c6ed7990f4e5f34f8632f8bcfcbb57
Keiyu Watanabe <cython_erizabass@icloud.com>
2025-01-31 11:39:37 +0900

    ぼやけるアニメーションを追加しました。

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const catchphrase = document.querySelector('.catchphrase');
+    const backgroundImage = document.querySelector('.background-image');
+    
+    catchphrase.classList.add('slide-in');
+    backgroundImage.classList.add('blur');
+});
