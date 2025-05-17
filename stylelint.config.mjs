276cd74d6b287f0f955157bf968b6d54998499e2
l1npengtul <l1npengtul@protonmail.com>
2025-05-17 20:15:31 +0900

    stylelint 2

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,24 +1,5 @@
-{
-    "extends": "stylelint-config-standard",
-    "rules": {
-    "indentation": 4,
-        "string-quotes": "double",
-        "no-duplicate-selectors": true,
-        "color-hex-case": "lower",
-        "color-hex-length": "short",
-        "color-named": "always-where-possible",
-        "declaration-no-important": true,
-        "declaration-colon-space-before": "never",
-        "declaration-colon-space-after": "always",
-        "property-no-vendor-prefix": true,
-        "value-no-vendor-prefix": true,
-        "number-leading-zero": "always",
-        "function-url-quotes": "always",
-        "function-url-data-uris": "never",
-        "at-rule-no-vendor-prefix": true,
-        "rule-empty-line-before": "always",
-        "selector-no-vendor-prefix": true,
-        "selector-no-universal": true,
-        "selector-no-type": true,
-        "media-feature-name-no-vendor-prefix": true
-}
+/** @type {import('stylelint').Config} */
+export default {
+    rules: {
+    }
+};
